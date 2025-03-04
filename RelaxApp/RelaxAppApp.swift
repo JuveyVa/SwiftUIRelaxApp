b25c320b2aef6362d88b058e160772465a1363d9
ITIT <ITIT@ULSAs-iMac.local>
2025-03-03 19:17:06 -0600

    se crea proyecto relax app en swift

---
+++ RelaxApp/RelaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxAppApp.swift
+//  RelaxApp
+//
+//  Created by ITIT on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RelaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
